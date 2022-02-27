b76571ce3722fab41d880d0e71051c0f6b1f66ec
getteli <douglas_araujo018@outlook.com>
2022-02-27 12:33:54 -0300

    remover mais emojis

--- app/Aux/Codes.php
+++ app/Aux/Codes.php
@@ -92,25 +92,28 @@ abstract class Codes
 		// Match Emoticons
 		$regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
 		$clear_string = preg_replace($regex_emoticons, '', $string);
-	
+
 		// Match Miscellaneous Symbols and Pictographs
 		$regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
 		$clear_string = preg_replace($regex_symbols, '', $clear_string);
-	
+
 		// Match Transport And Map Symbols
 		$regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
 		$clear_string = preg_replace($regex_transport, '', $clear_string);
-	
+
 		// Match Miscellaneous Symbols
 		$regex_misc = '/[\x{2600}-\x{26FF}]/u';
 		$clear_string = preg_replace($regex_misc, '', $clear_string);
-	
+
 		// Match Dingbats
 		$regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
 		$clear_string = preg_replace($regex_dingbats, '', $clear_string);
-	
+
 		// outras merdas q nao sai nem por um caraio inferno
 		$regex_saiCapeta = '/[\x{1F957}]/u';
+		$clear_string = preg_replace($regex_saiCapeta, '', $clear_string);
+
+		$regex_saiCapeta = '/[\x{1F9E1}-\x{2764}-\x{FE0F}-\x{1F49B}-\x{1F49A}-\x{1F499}-\x{1F49C}-\x{1F90E}-\x{1F5A4}-\x{1F90D}]/u';
 		$clear_string = preg_replace($regex_saiCapeta, '', $clear_string);
 
 		return $clear_string;
